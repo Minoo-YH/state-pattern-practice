0e838fba1f9a86aac029b8ec859a5d4205dc1140
Minoo Yaghoubi <155952014+Minoo-YH@users.noreply.github.com>
2026-09-22 23:12:56 +0300

    Update payment state transition

--- src/state/PaymentState.java
+++ src/state/PaymentState.java
@@ -2,8 +2,15 @@ package state;
 
 public class PaymentState implements State {
 
+    private Machine machine;
+
+    public PaymentState(Machine machine) {
+        this.machine = machine;
+    }
+
     @Override
     public void action() {
         System.out.println("Waiting for payment");
+        machine.setState(new PrintingState(machine));
     }
 }
