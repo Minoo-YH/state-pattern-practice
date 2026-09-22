dfeb651c56166e5aaff6a71692fef7dea51bb6eb
Minoo Yaghoubi <155952014+Minoo-YH@users.noreply.github.com>
2026-09-22 23:13:07 +0300

    Update State pattern example

--- src/state/Main.java
+++ src/state/Main.java
@@ -1,13 +1,16 @@
 package state;
 
 public class Main {
-    public static void main(String[] args) {
-        Machine machine = new Machine();
-        machine.setState(new ReadyState(machine));
-        machine.action();
-        machine.action();
-        machine.setState(new PaymentState());
-        machine.action();
 
+    public static void main(String[] args) {
+
+        Machine machine = new Machine();
+
+        machine.setState(new ReadyState(machine));
+
+        machine.action();
+        machine.action();
+        machine.action();
+        machine.action();
     }
 }
